949dede16f6e37e46f99efd7eb0b322146286b78
Vendicated <vendicated@riseup.net>
2025-12-02 21:30:40 +0100

    add bench arg

--- 2025/run.rb
+++ 2025/run.rb
@@ -2,16 +2,18 @@
 
 require "date"
 
+bench = ARGV.include? "--bench"
+if ARGV[0] == "--bench"
+    ARGV.shift
+end
+
 day = "day#{ARGV[0] || Date.today.day}"
 
-commands = {
-    "cr" => "crystal",
-    "rb" => "ruby"
-}
 
-for ext, command in commands.entries do
-    filename = "solution.#{ext}"
-    next unless File.exist? "#{day}/#{filename}"
-
-    system "#{command} #{filename}", :chdir => day
+if bench
+    system "crystal build --release solution.cr", :chdir => day
+    system "./solution", :chdir => day
+    system "hyperfine ./solution", :chdir => day
+else
+    system "crystal solution.cr", :chdir => day
 end
